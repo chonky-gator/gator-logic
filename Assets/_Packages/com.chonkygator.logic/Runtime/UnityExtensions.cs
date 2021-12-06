2049ded25509bf962b7af90779708a3cbad7fe3a
Tomás Rinaldi <tomi901@users.noreply.github.com>
2021-12-05 21:24:30 -0300

    Added StartCoroutine with Func<IEnumerator>

--- Assets/_Packages/com.chonkygator.logic/Runtime/UnityExtensions.cs
+++ Assets/_Packages/com.chonkygator.logic/Runtime/UnityExtensions.cs
@@ -1,14 +1,20 @@
 using System;
+using System.Collections;
+using UnityEngine;
 using Object = UnityEngine.Object;
 
 namespace GatOR.Logic
 {
-    public static class Utils
+    public static class UnityExtensions
     {
         public static T Or<T>(this T target, T ifNullOrDestroyed) where T : Object =>
             target ? target : ifNullOrDestroyed;
 
         public static T Or<T>(this T target, Func<T> ifNullOrDestroyed) where T : Object =>
             target ? target : ifNullOrDestroyed();
+
+        public static Coroutine StartCoroutine(this MonoBehaviour monoBehaviour,
+            Func<IEnumerator> enumeratorFunction) =>
+            monoBehaviour.StartCoroutine(enumeratorFunction());
     }
 }
