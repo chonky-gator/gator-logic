106ffb980ef704ea128485512ef33185e6cde40c
Tomás Rinaldi <tomi901@users.noreply.github.com>
2022-05-30 14:45:39 -0300

    Added OrThrowNullArgument

--- Runtime/UnityExtensions.cs
+++ Runtime/UnityExtensions.cs
@@ -13,6 +13,9 @@ namespace GatOR.Logic
         public static T Or<T>(this T target, Func<T> ifNullOrDestroyed) where T : Object =>
             target ? target : ifNullOrDestroyed();
 
+        public static T OrThrowNullArgument<T>(this T target, string argName) where T : Object =>
+            target ? target : throw new ArgumentNullException(argName);
+
         public static Coroutine StartCoroutine(this MonoBehaviour monoBehaviour,
             Func<IEnumerator> enumeratorFunction) =>
             monoBehaviour.StartCoroutine(enumeratorFunction());
