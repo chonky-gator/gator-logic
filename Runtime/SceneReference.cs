64f7ed5414f92ed6cbdfe9aeb0e04ed9bdd6ce78
Tomás Rinaldi <tomi901@users.noreply.github.com>
2022-08-23 14:03:04 -0300

    Added SceneReference to reference addressable scenes

---
+++ Runtime/SceneReference.cs
@@ -0,0 +1,19 @@
+﻿#if PACKAGE_ADDRESSABLES
+using UnityEngine.AddressableAssets;
+
+namespace GatOR.Logic
+{
+	[System.Serializable]
+	public class SceneReference :
+#if UNITY_EDITOR
+		AssetReferenceT<UnityEditor.SceneAsset>
+#else
+		AssetReference
+#endif
+	{
+		public SceneReference(string guid) : base(guid)
+		{
+		}
+	}
+}
+#endif
