30f6c5584f110ffa0536c3dec4cf574553e0911c
Tomás Rinaldi <tomi901@users.noreply.github.com>
2022-08-23 13:23:42 -0300

    IConstructable renaming

--- Runtime/IConstructable.cs
+++ Runtime/IConstructable.cs
@@ -6,32 +6,32 @@ namespace GatOR.Logic
         bool IsConstructed { get; }
     }
 
-    public interface IConstructable<T1> : IConstructable
+    public interface IConstructable<in T1> : IConstructable
     {
         void Construct(T1 arg1);
     }
     
-    public interface IConstructable<T1, T2> : IConstructable
+    public interface IConstructable<in T1, in T2> : IConstructable
     {
         void Construct(T1 arg1, T2 arg2);
     }
 
-    public interface IConstructable<T1, T2, T3> : IConstructable
+    public interface IConstructable<in T1, in T2, in T3> : IConstructable
     {
         void Construct(T1 arg1, T2 arg2, T3 arg3);
     }
 
-    public interface IConstructable<T1, T2, T3, T4> : IConstructable
+    public interface IConstructable<in T1, in T2, in T3, in T4> : IConstructable
     {
         void Construct(T1 arg1, T2 arg2, T3 arg3, T4 arg4);
     }
 
-    public interface IConstructable<T1, T2, T3, T4, T5> : IConstructable
+    public interface IConstructable<in T1, in T2, in T3, in T4, in T5> : IConstructable
     {
         void Construct(T1 arg1, T2 arg2, T3 arg3, T4 arg4, T5 arg5);
     }
 
-    public static class IConstructableExtensions
+    public static class ConstructableExtensions
     {
         public static void ThrowIfAlreadyConstructed(this IConstructable constructable)
         {
